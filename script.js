fc3215ebebcddddef16b07fd057b3f0315085707
SteveRobert7 <141488857+SteveRobert7@users.noreply.github.com>
2024-01-13 16:39:51 +0530

    nam-script.js

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+    const developers = arr
+        .filter((employee) => employee.profession === "developer")
+        .map((developer) => {
+            console.log(developer);
+            return developer;
+        });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+
+   arr.forEach((employee) => {
+        if (employee.profession === "developer") {
+            console.log(employee);
+        }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newEmployee);
+    console.log(newEmployee);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+
+  arr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  const newArray = [
+        { id: 5, name: "ajay", age: "27", profession: "Teacher" },
+        { id: 6, name: "Sachin", age: "22", profession: "Driver" },
+        { id: 7, name: "Rajiv", age: "28", profession: "Manager" },
+    ];
+
+    const combinedArray = arr.concat(newArray);
+    console.log(combinedArray);
+
 }
